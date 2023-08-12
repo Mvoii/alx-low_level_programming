d2646bfe7cc4b4e9fdb3dc0b267b651297f57ae0
Mvoii <106028842+Mvoii@users.noreply.github.com>
2023-08-12 17:36:09 +0300

    betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	/*prints in smalll letters*/
 	for (char ch = 'a'; ch<= 'z'; ch++)
     	{
-			putchar(ch);
-		}
+		putchar(ch);
+	}
 
 	/*prints in caps*/
 	for (char ch = 'A'; ch <= 'Z'; ch++)
-		{
-			putchar(ch);
-		}
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	
 	return (0);
