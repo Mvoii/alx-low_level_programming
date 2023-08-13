944e7b2a15b63625046ed0d641e02977e2ea298c
Mvoii <106028842+Mvoii@users.noreply.github.com>
2023-08-13 13:56:35 +0300

    Update 4-puts.c - betty style code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-  puts("\"Pogramming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Pogramming is like building a multilingual puzzle");
+	return (0);
 }
