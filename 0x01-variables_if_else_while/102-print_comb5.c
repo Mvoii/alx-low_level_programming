c6285817bd22a9d572a11cdb8c86375bbf731e4f
Mvoii <106028842+Mvoii@users.noreply.github.com>
2023-08-13 16:29:11 +0300

    Update 102-print_comb5.c-filled cadaver

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,40 @@
 #include<stdio.h>
 
-
+/**
+ * main - Entry Point
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
+ */
 
 int main(void)
 {
-  return (0);
+	int firstDigit = 0, secDigit;
+
+	while (firstDigit <= 99)
+	{
+		secDigit = first Digit;
+		while (secDigit <= 99)
+		{
+			if (secDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) + 48);
+				putchar((seconDigit % 10) + 48);
+
+				if (firstDigit != 98 || seconDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++seconDigit;
+		}
+		++firstDigit;
+	}
+	putchar('\n');
+
+	return (0);
 }
