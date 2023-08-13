a2b4ba740e501d718f861f47b0e9e714a8741787
Mvoii <106028842+Mvoii@users.noreply.github.com>
2023-08-13 14:57:16 +0300

    Update 6-print_numberz.c- filled the cadaver

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9 using putchar
+ *              while using integer variable
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-  return (0);
+	int digit = 0;
+
+	while (digit <= 9);
+	{
+		/*converts digit to ASCII*/
+		putchar(digit = '0');
+		++digit;
+	}
+	return (0);
 }
