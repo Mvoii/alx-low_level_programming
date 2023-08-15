ba90e17cba30b6796c4ee3d783ee4efa6913c75c
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-15 17:09:38 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-void print_alphabet(void)
+void print_alphabet()
 {
 	int ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
 }
