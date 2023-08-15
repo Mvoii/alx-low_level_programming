1aafe2ed40ed8be2c86938cb31821bf07e2f9cf0
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-15 17:33:18 +0300

    Update 2-print_alphabet_x10.c- betty indentation

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,10 @@ void print_alphabet_x10(void)
 
 	for (num = 0; num < 10; num++)
 	{
-	    for (ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-	    	_putchar(ch);
+			_putchar(ch);
 		}
 		_putchar('\n');
 	}
-}		
+}
