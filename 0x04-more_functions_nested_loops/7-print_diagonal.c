e63de0bf8da8f65592d53cfbc92a0ef10fd255d1
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-17 23:53:58 +0300

    Update 7-print_diagonal.c - added code logic and description

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,26 @@
 #include"main.h"
 
-
+/**
+ * print_diagonal - print a diagonal line
+ *
+ * @n: is the number of times the \ character
+ *     should be printed
+*/
 
 void print_diagonal(int n)
+{
+	int num, spaces;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (num = 0; num < n; num++)
+		{
+			for (spaces = 0; sapces <= num; spaces++)
+				_putchar(32); /*ascii value of blank space*/
+			_putchar(92); /*ascii value of slash*/
+			_putchar("\n');
+		}
+	}
+}
