b9942d38f9cbf2aa86b892328d0f28c696413ae5
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-17 23:33:55 +0300

    Update 6-print_line.c  added code logic and description

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,24 @@
 #include "main.h"
 
-
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ *     should be printed
+*/
 
 void print_line(int n)
+{
+	int num;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (num = 0; num < n; num++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
