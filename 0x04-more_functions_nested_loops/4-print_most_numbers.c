a1892f3a16940f45f86350ce4f2d21d9b571eb5f
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-17 22:11:59 +0300

    Update 4-print_most_numbers.c - added code logic and description

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,22 @@
 #include"main.h"
 
-
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
+*/
 
 void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		/* skips 2 and 4*/
+		if (number == 2 || number == 4)
+			continue;
+		_putchar(number + 48);
+	}
+	_putchar('\n');
+}
