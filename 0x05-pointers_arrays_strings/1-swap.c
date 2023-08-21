3490460a01283fb62713b3c0f3ed380b586878f9
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-21 15:34:54 +0300

    Update 1-swap.c - added code logic and description

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
-
+/**
+ * swap_int - swaps the values of two integers
+ *		using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+*/
 
 void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
