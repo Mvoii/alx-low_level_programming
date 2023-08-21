900e294aeae7a715de76d14b41bd1b58402ce8be
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-21 19:48:41 +0300

    Update 6-puts2.c - added code logic and decription

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
-
+/**
+ * puts2 - prints every character of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Noting
+*/
 
 oid puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
