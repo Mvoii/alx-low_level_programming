7c0a2949e6befa02e0a08a4de8afdd702d2bcf34
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-21 20:04:20 +0300

    Create 7-prints_half.c - added code logic and description

---
+++ 0x05-pointers_arrays_strings/7-prints_half.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void puts_half(char *str)
+{
+	int len, n;
+
+	for (len = 0; str[len] != '\0'; ++len)
+		;
+
+	if (len % 2 == 0)
+	{
+		for (n = len / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	} else
+	{
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
