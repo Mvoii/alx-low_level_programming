990964f58da46110577ad097e1fb2c7dec4f092f
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-21 16:33:04 +0300

    Update 2-strlen.c -added code logic and description

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,19 @@
 #include "main.h"
 
-
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
 
 int _strlen(char *s)
+{
+	int len = 0;
+	for (*str; *str != '\0'; *str++)
+	{
+		len++;
+	}
+	return (len);
+}
