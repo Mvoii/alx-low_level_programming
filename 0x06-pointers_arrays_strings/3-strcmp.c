b0f4ab80562a7277c43ce3f9883aee301c266367
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-24 21:51:37 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,33 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0, r;
 
-	i = 0;
+	/**
+	 * iterate through src and
+	 * compare it with dest
+	*/
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		/**
+		 * if they differ by a single character
+		 * don't iterate further
+		*/
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			/*
+			 * return the difference between
+			 * the two characters
+			*/
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
 		}
 		i++;
 	}
-	return (0);
+
+	return (r);
 }
