e1a726427fe06fe625f79fe7ba775e0ac30e7319
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-24 18:55:50 +0300

    Update 4-rev_array.c - added code logic and description

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,27 @@
 #include "main.h"
 
+/**
+ * reverse_array - a function that reverses the
+ *                content of an array of integers
+ *
+ * @a: pointer to int array
+ * @n: is the number of elements to swap
+ *
+ * Return: nothing
+*/
 
 void reverse_array(int *a, int n)
+{
+	int start = 0;
+
+	int end = n - 1;
+
+	for (start; start < end; start++)
+	{
+		int tmp = a[start];
+		a[start] = a[end];
+		a[end] = tmp;
+
+		end--;
+	}
+}
