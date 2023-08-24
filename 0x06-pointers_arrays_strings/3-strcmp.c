d9dd8c8e1b94f973edb3dda65ad91cb76a457c03
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-24 21:49:32 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int n;
+	int i;
 
-	n = 0;
-	while (s1[n] != '\0' && s2[n] != '\0')
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[n] != s2[n])
+		if (s1[i] != s2[i])
 		{
-			return (s1[n] - s2[n]);
+			return (s1[i] - s2[i]);
 		}
-		n++;
+		i++;
 	}
 	return (0);
 }
