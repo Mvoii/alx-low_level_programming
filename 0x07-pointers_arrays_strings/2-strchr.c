3d39fcd04525478ee6a8930747c96809ab95787d
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-28 20:53:38 +0300

    Update 2-strchr.c - added code logic

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,20 @@
 #include "main."
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *f;
+
+	for (i = 0; i < (strlen(s) + 1); i++)
+	{
+		if (c == s[i])
+		{
+            for (int j = 0; i < strlen(s); j++)
+            {
+                f[j] = s[i];
+                i++;
+            }
+            return (f);
+        }
+    }
+    return ('\0';
+}
