536bdb683f5f1a9774c00bd63e383be72679db3a
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-28 17:39:10 +0300

    Update 0-memset.c - added code logic and description

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,14 @@
 #include "main.h"
+/*
+ * sets the memory address to a given address
+ * returns (0)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+    int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+}
