29323b2882b5f71d67321aad4e1009196c971ed5
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-28 21:01:03 +0300

    Update 2-strchr.c - major code update - total revamp

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,27 @@
-#include "main."
+/**
+ * _strchr - a function that locates a character in a string
+ *
+ * @s: pointer to our string array input
+ * @c: character to locate from input array
+ *
+ * Return: first occurence of charatcer or null if not found
+*/
+
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-	char *f;
-
-	for (i = 0; i < (strlen(s) + 1); i++)
+	while (*s != '\0')
 	{
-		if (c == s[i])
-		{
-            for (int j = 0; i < strlen(s); j++)
-            {
-                f[j] = s[i];
-                i++;
-            }
-            return (f);
-        }
-    }
-    return ('\0';
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	/**
+	 * if c is '\0', you should return
+	 * the pointer to the '\0' of the
+	 * string s
+	*/
+	if (*s == c)
+		return (s);
+	/*return null if not found*/
+	return ('\0');
 }
