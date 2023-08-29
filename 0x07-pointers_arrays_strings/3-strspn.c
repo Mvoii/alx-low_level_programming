0ffed13301e8ea67269bfd7d9db59886b7cef0b5
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-29 17:08:32 +0300

    Update 3-strspn.c - major code revamp

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,31 @@
 #include "main.h"
 
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	int i, j;
 
 	i = 0;
 
-	while (s[i] == accept[i])
-		i++;
-
-	return i;
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				n++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (i);
+		}
+		s++;
+	}
+	return (i);
 }
