b198c4b86690d17d02308c3ade15434c5963da6c
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-30 19:30:49 +0300

    Update 0-puts_recursion.c - fixed bug line 10 and 16

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- * main - check the code
- * @char *s
+ * _puts_recursion - a function that pirnts a new line
+ * @s: the string
+ * 
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
 	/*code description*/
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
