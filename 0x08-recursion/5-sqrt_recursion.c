0bfd749f5845e40c68a0a314b57aecfa3c8cde76
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-08-30 23:23:05 +0300

    Update 5-sqrt_recursion.c - added alt sqrt function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,13 +21,14 @@ int _sqrt_recursion(int n)
  * @i: iterator
  *
  * Return: the resulting square root
- */
+ 
 
-int actual_sqrt_recursion(int n, int i)
-{
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
-}
+ * int actual_sqrt_recursion(int n, int i)
+ * {
+ *	if (i * i > n)
+ *		return (-1);
+ * 	if (i * i == n)
+ * 		return (i);
+ * 	return (actual_sqrt_recursion(n, i + 1));
+ * }
+*/
