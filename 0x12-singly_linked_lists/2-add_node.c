befa1acc25bdc04c66f0be0a2964e8a195b7b4bf
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-09-22 22:18:04 +0300

    Update 2-add_node.c - another minor tweak

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,10 +19,21 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
 	new_node->str = strdup(str);
 	new_node->len = str_len;
-	new_node->next = (*head);
-	(*head) = new_node;
+	*head = new_node;
 
 	return (*head);
 }
