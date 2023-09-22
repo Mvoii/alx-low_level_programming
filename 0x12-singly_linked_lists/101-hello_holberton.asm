41178da24909b5c12b641c8043e1d8b7348dba89
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-09-22 23:17:55 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,22 @@
 ;I have no idea what I'm doing
 	extern  printf	;the function to be called
 
-	section .data	;data section containg variables to be initialized
-	call	printf	;call printf function
+	section .data		; Data section, variables to be intialized
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
+
+	section .text		; Code section.
+
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be alligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
