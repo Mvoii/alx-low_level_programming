94f70642d07dfd4a17dce367d2ad02b35b3a0103
Mvoi <106028842+Mvoii@users.noreply.github.com>
2023-09-22 22:26:29 +0300

    Update 2-add_node.c - minor bug

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[str_len])
 		str_len++;
 
-	new_node = malloc(sizeof(list_t);
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
