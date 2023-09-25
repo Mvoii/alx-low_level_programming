98aedbcfbd21e0ba743a13b905e8e2b331117e42
mvoi <mvoifranklin@gmail.com>
2023-09-25 11:46:43 -0700

    added file to delete node at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * int delete_nodeint_at_index - deletes the node at given position
+ *
+ * @index: the position of nodeint to be deleted
+ *
+ * Return: 1 when successful, -1 when failed
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
