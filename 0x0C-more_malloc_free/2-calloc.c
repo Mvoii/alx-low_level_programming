5c3df99715a0fb06d647e7205a7a57b9173a1766
Mvoi <mvoifranklin@gmail.com>
2023-11-02 21:51:34 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,44 +1,26 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of characters to be copied
- * Return: copied
+ * _calloc - allocate memory for an array
+ * @nmemb: number of elements in an array
+ * @size: size on the array
+ * Return: pointer string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int s1_len = 0, s2_len = 0, i;
-	char *s;
+	unsigned int i;
+	char *p;
 
-	/* if NULL is passed, treat as an empty string */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	/* While at index 0 */
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (s2[s2_len] != '\0')
-		s2_len++;
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	/* Memory allocation */
-	s = malloc(sizeof(char) * n + s1_len + 1);
-	if (s == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	for (i = 0; i < s1_len; i++)
-		s[i] = s1[i];
+	p = malloc(nmemb * size);
 
-	for (i = 0; i < n; i++)
-		s[i + s1_len] = s2[i];
+	if (p == NULL)
+		return (NULL);
 
-	s[i + s1_len] = '\0';
+	for (i = 0; i < nmemb * size; i++)
+		*(p + i) = 0;
 
-	return (s);
+	return (p);
 }
