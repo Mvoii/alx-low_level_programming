6fc757c2784b1e75b0b3e8650debe18b9e1554b7
Mvoi <mvoifranklin@gmail.com>
2023-11-08 23:00:44 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -17,4 +16,4 @@ typedef enum bool
 	FALSE = 0
 } bool;
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
