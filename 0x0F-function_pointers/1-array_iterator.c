b0d68c4502a017374a5073e91bc1f8d3ae89768d
Mvoi <mvoifranklin@gmail.com>
2023-11-08 23:09:30 +0300

    Update 1-array_iterator.c - minor bug

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
  * Return: empty if @array and @action is NULL
 *//
 
-void array_iterator(int *array, size_t_size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int index;
 
