82a6cd62238cff888ee15b542151c0116319e077
franklin mvoi <mvoifranklin@gmail.com>
2023-11-27 05:01:34 -0800

    code revamp

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,18 +12,34 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *buff;
+	int fd;
+	ssize_t written, readfile;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	readfile = read(fd, buff, letters);
+
+	written = write(fd, buff, letters);
+
+	if (fd == -1 || readfile == -1 || written != readfile)
+	{
+		free(buff);
+		close(fd);
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-	free(buf);
+	}
+
+	free(buff);
+
 	close(fd);
-	return (w);
+
+	return (written);
 }
