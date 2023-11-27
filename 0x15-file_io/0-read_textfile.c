61b1a20ad6a5883570eef57168ef13304a6934d6
franklin mvoi <mvoifranklin@gmail.com>
2023-11-27 04:00:42 -0800

    added cadaver 0-read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * @filename - name of file to be read
+ *
+ * @letters - number of letters to be printed
+ *
+ * Return: number of letters to be printed
+ *
+*/ 
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	return (0);
+}
