1f964de9db81ae237ca0c80b1017a3f1c0d0934f
Mvoi <mvoifranklin@gmail.com>
2023-11-27 19:40:12 +0300

    Update 1-create_files.c - semicolon missing found

--- 0x15-file_io/1-create_files.c
+++ 0x15-file_io/1-create_files.c
@@ -27,6 +27,6 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (write(ptr, text_content, strlen(text_content)) == (ssize_t) - 1)
 		return (-1);
-	close(ptr)
+	close(ptr);
 	return (1);
 }
