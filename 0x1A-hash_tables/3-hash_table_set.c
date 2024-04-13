7c9b4d9cdc4842c58fa730a8d7a4e02a74828fa0
Franklin Mvoi <mvoifranklin@gmail.com>
2024-04-13 18:02:47 +0300

    anpthe fix  3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,55 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
  * hash_table_set - adds an element to the hash table
- * @ht: hash table to add element to
- * @key: key to add
- * @value: value to add
- * Return: 1 if successful, 0 otherwise
+ * @ht: hash table you want to add or update the key/value to
+ * @key: the key (can not be an empty string)
+ * @value: value associated with the key
+ * value must be duplicated. value can be an empty string
+ *
+ * Return: 1 if it succeeded || 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index, i;
-    hash_node_t *new_node = NULL;
-    char *new_value = NULL;
+	hash_node_t *new;
+	unsigned long int i, index;
+	char *new_value;
 
-    if (ht == NULL || key == NULL || value == NULL)
-        return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-    new_value = strdup(value);
-    if (new_node == NULL)
-        return (0);
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    for (i = index; ht->array[i]; i++)
-    {
-        if (strcmp(ht->array[i]->key, key) == 0)
-        {
-            free(ht->array[i]->value);
-            ht->array[i]->value = new_value;
-            return (1);
-        }
-    }
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-    {
-        free(new_value);
-        return (0);
-    }
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
-    new_node->value = new_value;
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-    return (1);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = new_value;
+			return (1);
+		}
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(new_value);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = new_value;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,30 +1,33 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
  * hash_table_get - retrieves a value associated with a key
- * @ht: hash table to retrieve value from
- * @key: key to retrieve value of
- * Return: value associated with key, or NULL if key not found
+ * @ht: hash table to look into
+ * @key: key being looked for
+ *
+ * Return: value associated with the element || NULL if key couldn’t be found
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *node;
-    unsigned long int index = 0;
+	hash_node_t *node;
+	unsigned long int index = 0;
 
-    if (ht == NULL)
-        return (NULL);
+	if (ht == NULL)
+		return (NULL);
 
-    index = key_index((unsigned char *)key, ht->size);
-    node = ht->array[index];
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-    if (node == NULL)
-        return (NULL);
+	if (node == NULL)
+		return (NULL);
 
-    while (node)
-    {
-        if (!strcmp(node->key, key))
-            return (node->value);
-        node = node->next;
-    }
-    return (NULL);
+	while (node)
+	{
+		if (!strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
