451c7138babda4dd2048a8dc50208ac2945bab6a
Franklin Mvoi <mvoifranklin@gmail.com>
2024-05-29 15:10:59 +0300

    island fixer

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""defines an island preimeter measuring function"""
+"""Defines an island perimeter measuring function"""
+
 
 def island_perimeter(grid):
-    """returns the perimeter of an island"""
-    perimeter = 0
+    """
+    Return the perimeter of an island described in grid
+    args: grid(list) - a list of lists of integers
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
