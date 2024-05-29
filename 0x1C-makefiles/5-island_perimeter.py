11d6deba12bc0accb88ea6704827b9e5f464368d
Franklin Mvoi <mvoifranklin@gmail.com>
2024-05-29 14:50:17 +0300

    pystyling

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@
 def island_perimeter(grid):
     """returns the perimeter of an island"""
     perimeter = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
